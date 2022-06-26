ab7d717f45dcf4522358dbbe7461af4a6f375cb8
PoChang <hpochang@poChangDeMacBookPro.local>
2022-06-27 02:06:31 +0800

    訂單欄位改中文

--- orders/forms.py
+++ orders/forms.py
@@ -7,6 +7,14 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        labels = {
+            'first_name': '名字',
+            'last_name': '姓',
+            'email': '電子郵件',
+            'address': '地址',
+            'postal_code': '郵遞區號',
+            'city': '城市'
+        }
         widgets = {
         'first_name': forms.TextInput(attrs={'class': 'form-control'}),
         'last_name': forms.TextInput(attrs={'class': 'form-control'}),
