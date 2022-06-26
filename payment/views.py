269eedad62e990767f8eb179b3bdff1536f2f0bd
PoChang <hpochang@poChangDeMacBookPro.local>
2022-06-27 03:56:46 +0800

    website title

--- payment/views.py
+++ payment/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from paypal.standard.forms import PayPalPaymentsForm
+# from paypal.standard.forms import PayPalPaymentsForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -18,17 +18,19 @@ def payment_process(request):
     total_cost = order.get_total_cost()
     host = request.get_host()
 
-    paypal_dict = {
-        # 'business': settings.PAYPAL_RECEIVER_EMAIL,
-        'amount': '%.2f' % order.get_total_cost().quantize(Decimal('.01')),
-        'item_name': 'Order {}'.format(order.id),
-        'invoice': str(order.id),
-        'currency_code': 'TWD',
-        # 'notify_url': 'http://{}{}'.format(host, reverse('paypal-ipn')),
-        'return_url': 'http://{}{}'.format(host, reverse('payment:done')),
-        'cancel_return': 'http://{}{}'.format(host, reverse('payment:canceled')),
-    }
-    form = PayPalPaymentsForm(initial=paypal_dict)
+    # paypal_dict = {
+    #     # 'business': settings.PAYPAL_RECEIVER_EMAIL,
+    #     'amount': '%.2f' % order.get_total_cost().quantize(Decimal('.01')),
+    #     'item_name': 'Order {}'.format(order.id),
+    #     'invoice': str(order.id),
+    #     'currency_code': 'TWD',
+    #     # 'notify_url': 'http://{}{}'.format(host, reverse('paypal-ipn')),
+    #     'return_url': 'http://{}{}'.format(host, reverse('payment:done')),
+    #     'cancel_return': 'http://{}{}'.format(host, reverse('payment:canceled')),
+    # }
+    # form = PayPalPaymentsForm(initial=paypal_dict)
+
+    
     return render(request, 'payment/process.html', {'order': order, 'form': form})
 
 
