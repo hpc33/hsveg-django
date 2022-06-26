40357e76cfac82722a7e20000baad3d660256f43
PoChang <hpochang@poChangDeMacBookPro.local>
2022-06-26 10:41:34 +0800

    first commit

---
+++ payment/ecpay.py
@@ -0,0 +1,141 @@
+from django.shortcuts import render, get_object_or_404
+from orders.models import Order
+import importlib.util
+spec = importlib.util.spec_from_file_location(
+    "ecpay_payment_sdk",
+    "payment/ecpay_payment_sdk.py"
+)
+from datetime import datetime
+
+
+
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+def ecpay_main(order_id):
+    order = get_object_or_404(Order, id=order_id)
+    price = order.get_total_cost()
+
+    order_params = {
+        'MerchantTradeNo': datetime.now().strftime("NO%Y%m%d%H%M%S"),
+        'StoreID': '',
+        'MerchantTradeDate': datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
+        'PaymentType': 'aio',
+        'TotalAmount': price,
+        'TradeDesc': '訂單測試',
+        'ItemName': str(order.id),
+        'ReturnURL': '', # 顧客填完付款資料送出後的頁面
+        'ChoosePayment': 'ALL',
+        'ClientBackURL': '',
+        'ItemURL': '',
+        'Remark': '交易備註',
+        'ChooseSubPayment': '',
+        'OrderResultURL': '',
+        'NeedExtraPaidInfo': 'Y',
+        'DeviceSource': '',
+        'IgnorePayment': '',
+        'PlatformID': '',
+        'InvoiceMark': 'N',
+        'CustomField1': '',
+        'CustomField2': '',
+        'CustomField3': '',
+        'CustomField4': '',
+        'EncryptType': 1,
+    }
+
+    extend_params_1 = {
+        'expireDate': 7,
+        'PaymentInfoURL': 'https://www.ecpay.com.tw/payment_info_url.php',
+        'ClientRedirectURL': '',
+    }
+
+    extend_params_2 = {
+        'StoreExpireDate': 15,
+        'Desc_1': '',
+        'Desc_2': '',
+        'Desc_3': '',
+        'Desc_4': '',
+        'PaymentInfoURL': 'https://www.ecpay.com.tw/payment_info_url.php',
+        'ClientRedirectURL': '',
+    }
+
+    extend_params_3 = {
+        'BindingCard': 0,
+        'MerchantMemberID': '',
+    }
+
+    extend_params_4 = {
+        'Redeem': 'N',
+        'UnionPay': '0',
+    }
+
+    inv_params = {
+        # 特店自訂編號
+        # 'RelateNumber': 'Xxx0001',
+        # 客戶編號
+        # 'CustomID': 'Xxx_0000001',
+        # 統一編酪
+        # 'CustomerIdentifier': '12345678',
+        # 'CustomerName': '客戶名稱',
+        # 'CustomerAddr': '客戶地址',
+        # 客戶手機號碼
+        # 'CustomerPhone': '0912345678',
+        # 客戶電子郵件
+        # 'CustomerEmail': 'user@example.com',
+        # 通關方式
+        # 'ClearanceMark': '2',
+        # 課稅類別
+        # 'TaxType': '1',
+        # 載具類別
+        # 'CarruerType': '',
+        # 捐贈註記
+        # 'Donation': '',
+        # 捐贈碼
+        # 'LoveCode': '168001',
+        # 'Print': '1',
+        # 'InvoiceItemName': '測試商品1|測試商品2',
+        # 'InvoiceItemCount': '2|3',
+        # 'InvoiceItemWord': '個|包',
+        # 'InvoiceItemPrice': '35|10',
+        # 'InvoiceItemTaxType': '1|1',
+        # 'InvoiceRemark': '測試商品1的說明|測試商品2的說明',
+        # 延遲天數
+        # 'DelayDay': '0',
+        # 字軌類別
+        # 'InvType': '07',
+    }
+
+    # 建立實體
+    ecpay_payment_sdk = module.ECPayPaymentSdk(
+        MerchantID='2000132',
+        HashKey='5294y06JbISpM5x9',
+        HashIV='v77hoKGq4kWxNNIS',
+    )
+
+    # 合併延伸參數
+    order_params.update(extend_params_1)
+    order_params.update(extend_params_2)
+    order_params.update(extend_params_3)
+    order_params.update(extend_params_4)
+
+    # 合併發票參數
+    order_params.update(inv_params)
+
+    try:
+        # 產生綠界訂單所需參數
+        final_order_params = ecpay_payment_sdk.create_order(order_params)
+        print('##############')
+        print(final_order_params)
+        print('##############')
+        # 產生 html 的 form 格式
+        action_url = 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5' # 測試環境
+        # action_url = 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5' # 正式環境
+        html = ecpay_payment_sdk.gen_html_post_form(action_url, final_order_params)
+        return html # print(html)
+    except Exception as error:
+        print('An exception happened: ' + str(error))
+
+
+
+
+
---
+++ payment/views.py
@@ -0,0 +1,77 @@
+from decimal import Decimal
+from django.conf import settings
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from paypal.standard.forms import PayPalPaymentsForm
+from django.http import HttpResponse
+
+# Create your views here.
+from orders.models import Order
+from .ecpay import ecpay_main
+from django.http import HttpResponseRedirect
+
+
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    total_cost = order.get_total_cost()
+    host = request.get_host()
+    paypal_dict = {
+        # 'business': settings.PAYPAL_RECEIVER_EMAIL,
+        'amount': '%.2f' % order.get_total_cost().quantize(Decimal('.01')),
+        'item_name': 'Order {}'.format(order.id),
+        'invoice': str(order.id),
+        'currency_code': 'TWD',
+        # 'notify_url': 'http://{}{}'.format(host, reverse('paypal-ipn')),
+        'return_url': 'http://{}{}'.format(host, reverse('payment:done')),
+        'cancel_return': 'http://{}{}'.format(host, reverse('payment:canceled')),
+    }
+    form = PayPalPaymentsForm(initial=paypal_dict)
+    return render(request, 'payment/process.html', {'order': order, 'form': form})
+
+
+@csrf_exempt
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
+
+
+@csrf_exempt
+def ecpay(request):
+    order_id = request.session.get('order_id')
+    return HttpResponse(ecpay_main(order_id))
+
+
+def successful_payment(request):
+    return render(request, 'payment/successful.html')
+
+
+def failure_payment(request):
+    return render(request, 'payment/failure.html')
+
+
+@csrf_exempt
+def end_page(request):
+    print('to user server')
+    if request.method == 'GET':
+        return HttpResponseRedirect(reverse('payment:fail'))
+    
+    if request.method == 'POST':
+        result = request.POST.get('RtnMsg')
+        print(result)
+        if result == 'Successed':
+            print('#######')
+            print(request.POST.get('TradeNo'))
+            print(request.POST.get('TradeAmt'))
+            print(request.POST.get('TradeDate'))
+            check = request.POST.get('CheckMacValue')
+            print(check)
+            print('#######')
+            return HttpResponseRedirect(reverse('payment:success'))
+        else:
+            return HttpResponseRedirect(reverse('payment:fail'))
+
+
+def end_return(request):
+    if request.method == 'POST':
+        return '1|OK'
