d68a33ed4d7e9d85d6fbf6b253d478e46a412695
PoChang <hpochang@poChangDeMacBookPro.local>
2022-06-27 07:03:17 +0800

    process 頁面調整 urls 調整

--- payment/urls.py
+++ payment/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('process/', views.payment_process, name='process'),
     path('successful/', views.successful_payment, name='done'),
     path('canceled/', views.payment_canceled, name='canceled'),
+    path('failed/', views.failure_payment, name='failed'),
+    path('end_page/', views.end_page, name='end_page'),
 ]
